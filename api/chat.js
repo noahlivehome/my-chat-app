7486de7a9feb619bdc7d103402bb0a4ba2451262
ノアリブホーム <info@noahlivehome.jp>
2026-07-25 13:24:25 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
 
     const options = {
       hostname: 'generativelanguage.googleapis.com',
-      path: `/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+     path: `/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
